ac6e0166340e88ad9d27bb1c1ddea4c889b201b6
Dindimon <dino.custic@outletcity.com>
2019-04-15 13:19:18 +0200

    vue added

---
+++ vue/websockets/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <WebSocket />
+  </div>
+</template>
+
+<script>
+  import WebSocket from './components/WebSocket.vue';
+
+export default {
+  name: 'app',
+  components: {
+    WebSocket
+  }
+}
+</script>
